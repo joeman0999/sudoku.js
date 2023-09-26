81c3c06af188596e48ff19ab69f3ce66a27fb26f
joeman0999 <ogliojoseph2@gmail.com>
2023-09-26 00:54:57 +0000

    "added packages back"

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile('/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+// const express = require('express');
+// const app = express();
+// const http = require('http');
+// const server = http.createServer(app);
+
+// app.get('/', (req, res) => {
+//   res.send('<h1>Hello world</h1>');
+// });
+
+// server.listen(3000, () => {
+//   console.log('listening on *:3000');
+// });
